cd18964b80f9d2f27633177689f92cdcf1b07cb9
Hariprasaadh <hariprasaadh.k2023@vitstudent.ac.in>
2025-03-06 01:00:38 +0530

    update

--- backend/atsresume/main.py
+++ backend/atsresume/main.py
@@ -20,10 +20,10 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Allows all origins
+    allow_origins=["*"],  
     allow_credentials=True,
-    allow_methods=["*"],  # Allows all methods
-    allow_headers=["*"],  # Allows all headers
+    allow_methods=["*"],  
+    allow_headers=["*"],  
 )
 
 load_dotenv()
