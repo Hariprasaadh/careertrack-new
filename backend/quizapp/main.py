9962887b9a3dea452ed3f7010ae87cf62976bb33
Hariprasaadh <hariprasaadh.k2023@vitstudent.ac.in>
2025-03-06 01:38:39 +0530

    add library

--- backend/quizapp/main.py
+++ backend/quizapp/main.py
@@ -6,6 +6,7 @@ from langchain_core.prompts import PromptTemplate
 from langchain_groq import ChatGroq
 from langchain_core.output_parsers import JsonOutputParser
 import os
+from fastapi.responses import HTMLResponse
 
 load_dotenv()
 
